de645575da48ea0ef6aa4ed48dc2004e829fa75b
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:35:16 -0700

    Build V5 foundation - calendar module

--- calendar.js
+++ calendar.js
@@ -1,1 +1,67 @@
+const Calendar = {
 
+    reminders: [],
+
+    load() {
+        this.reminders = Utils.load("calendar", []);
+    },
+
+    save() {
+        Utils.save("calendar", this.reminders);
+    },
+
+    create(data = {}) {
+
+        const reminder = {
+            id: Utils.id(),
+            title: "",
+            description: "",
+            eventId: "",
+            date: "",
+            time: "",
+            category: "General",
+            completed: false,
+            created: Utils.date(),
+            ...data
+        };
+
+        this.reminders.push(reminder);
+        this.save();
+
+        return reminder;
+    },
+
+    update(id, updates) {
+
+        const reminder = this.reminders.find(r => r.id === id);
+
+        if (!reminder) return;
+
+        Object.assign(reminder, updates);
+
+        this.save();
+    },
+
+    remove(id) {
+
+        this.reminders = this.reminders.filter(r => r.id !== id);
+
+        this.save();
+    },
+
+    get(id) {
+        return this.reminders.find(r => r.id === id);
+    },
+
+    all() {
+        return this.reminders;
+    },
+
+    upcoming() {
+        const today = new Date().toISOString().split("T")[0];
+        return this.reminders.filter(r => r.date >= today && !r.completed);
+    }
+
+};
+
+Calendar.load();
