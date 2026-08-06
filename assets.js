77ac71a8e8c5d531ac05f443b79d60ef6023de9c
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:33:24 -0700

    Build V5 foundation - assets module

--- assets.js
+++ assets.js
@@ -1,1 +1,72 @@
+const Assets = {
 
+    assets: [],
+
+    load() {
+        this.assets = Utils.load("assets", []);
+    },
+
+    save() {
+        Utils.save("assets", this.assets);
+    },
+
+    create(data = {}) {
+
+        const asset = {
+            id: Utils.id(),
+            name: "",
+            category: "",
+            serialNumber: "",
+            purchaseDate: "",
+            purchasePrice: 0,
+            currentValue: 0,
+            location: "",
+            assignedTo: "",
+            assignedEventId: "",
+            status: "Available",
+            condition: "Good",
+            warrantyExpires: "",
+            maintenanceDue: "",
+            notes: "",
+            ...data
+        };
+
+        this.assets.push(asset);
+        this.save();
+
+        return asset;
+    },
+
+    update(id, updates) {
+
+        const asset = this.assets.find(a => a.id === id);
+
+        if (!asset) return;
+
+        Object.assign(asset, updates);
+
+        this.save();
+    },
+
+    remove(id) {
+
+        this.assets = this.assets.filter(a => a.id !== id);
+
+        this.save();
+    },
+
+    get(id) {
+        return this.assets.find(a => a.id === id);
+    },
+
+    all() {
+        return this.assets;
+    },
+
+    available() {
+        return this.assets.filter(a => a.status === "Available");
+    }
+
+};
+
+Assets.load();
