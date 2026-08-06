93ad2ad0c9dbb8a90d311742e9b7ac090424ca0b
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 15:46:03 -0700

    Update service worker for V5

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,36 @@
+const CACHE = "snpplanner-v5";
 
+const FILES = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response =>
+            response || fetch(event.request)
+        )
+    );
+});
