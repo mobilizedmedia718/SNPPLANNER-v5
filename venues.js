be3663c81d897a037a71300102d3764ce798ed89
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:23:01 -0700

    Build V5 foundation - venues module

--- venues.js
+++ venues.js
@@ -1,1 +1,69 @@
+const Venues = {
 
+    list: [],
+
+    load() {
+        this.list = Utils.load("venues", []);
+    },
+
+    save() {
+        Utils.save("venues", this.list);
+    },
+
+    create(data = {}) {
+
+        const venue = {
+            id: Utils.id(),
+            name: "",
+            address: "",
+            city: "",
+            state: "",
+            zip: "",
+            phone: "",
+            email: "",
+            website: "",
+            capacity: 0,
+            rentalCost: 0,
+            deposit: 0,
+            depositRefundable: true,
+            parking: "",
+            indoorOutdoor: "Indoor",
+            notes: "",
+            ...data
+        };
+
+        this.list.push(venue);
+        this.save();
+
+        return venue;
+    },
+
+    update(id, updates) {
+
+        const venue = this.list.find(v => v.id === id);
+
+        if (!venue) return;
+
+        Object.assign(venue, updates);
+
+        this.save();
+    },
+
+    remove(id) {
+
+        this.list = this.list.filter(v => v.id !== id);
+
+        this.save();
+    },
+
+    get(id) {
+        return this.list.find(v => v.id === id);
+    },
+
+    all() {
+        return this.list;
+    }
+
+};
+
+Venues.load();
