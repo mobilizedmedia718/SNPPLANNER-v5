a417ce49de45a0c34de77c96015e5412fbbfd19e
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:28:46 -0700

    Build V5 foundation - inventory module
    Implemented inventory management functions including load, save, create, update, remove, get, all, and lowStock.

--- inventory.js
+++ inventory.js
@@ -1,1 +1,71 @@
+const Inventory = {
 
+    items: [],
+
+    load() {
+        this.items = Utils.load("inventory", []);
+    },
+
+    save() {
+        Utils.save("inventory", this.items);
+    },
+
+    create(data = {}) {
+
+        const item = {
+            id: Utils.id(),
+            name: "",
+            category: "",
+            vendorId: "",
+            sku: "",
+            unit: "Each",
+            quantity: 0,
+            minimum: 0,
+            reorder: 0,
+            cost: 0,
+            sellPrice: 0,
+            storageLocation: "",
+            notes: "",
+            active: true,
+            ...data
+        };
+
+        this.items.push(item);
+        this.save();
+
+        return item;
+    },
+
+    update(id, updates) {
+
+        const item = this.items.find(i => i.id === id);
+
+        if (!item) return;
+
+        Object.assign(item, updates);
+
+        this.save();
+    },
+
+    remove(id) {
+
+        this.items = this.items.filter(i => i.id !== id);
+
+        this.save();
+    },
+
+    get(id) {
+        return this.items.find(i => i.id === id);
+    },
+
+    all() {
+        return this.items;
+    },
+
+    lowStock() {
+        return this.items.filter(i => i.quantity <= i.minimum);
+    }
+
+};
+
+Inventory.load();
