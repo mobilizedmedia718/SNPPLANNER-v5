45402cb4ce15f14895156478e4cfc3160c5ba388
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:26:35 -0700

    Build V5 foundation - vendors module

--- vendors.js
+++ vendors.js
@@ -1,1 +1,69 @@
+const Vendors = {
 
+    list: [],
+
+    load() {
+        this.list = Utils.load("vendors", []);
+    },
+
+    save() {
+        Utils.save("vendors", this.list);
+    },
+
+    create(data = {}) {
+
+        const vendor = {
+            id: Utils.id(),
+            name: "",
+            category: "",
+            contact: "",
+            phone: "",
+            email: "",
+            website: "",
+            address: "",
+            city: "",
+            state: "",
+            zip: "",
+            paymentType: "Flat Rate",
+            flatRate: 0,
+            percentage: 0,
+            notes: "",
+            active: true,
+            ...data
+        };
+
+        this.list.push(vendor);
+        this.save();
+
+        return vendor;
+    },
+
+    update(id, updates) {
+
+        const vendor = this.list.find(v => v.id === id);
+
+        if (!vendor) return;
+
+        Object.assign(vendor, updates);
+
+        this.save();
+    },
+
+    remove(id) {
+
+        this.list = this.list.filter(v => v.id !== id);
+
+        this.save();
+    },
+
+    get(id) {
+        return this.list.find(v => v.id === id);
+    },
+
+    all() {
+        return this.list;
+    }
+
+};
+
+Vendors.load();
