532cb58df0793b65b3955fe8a7934af8659e1466
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:37:51 -0700

    Build V5 foundation - settings module

--- settings.js
+++ settings.js
@@ -1,1 +1,45 @@
+const Settings = {
 
+    data: {
+        theme: "light",
+        currency: "USD",
+        language: "en",
+        autosave: true,
+        notifications: true,
+        compactMode: false
+    },
+
+    load() {
+        this.data = Utils.load("settings", this.data);
+    },
+
+    save() {
+        Utils.save("settings", this.data);
+    },
+
+    update(key, value) {
+
+        if (!(key in this.data)) return;
+
+        this.data[key] = value;
+
+        this.save();
+    },
+
+    reset() {
+
+        this.data = {
+            theme: "light",
+            currency: "USD",
+            language: "en",
+            autosave: true,
+            notifications: true,
+            compactMode: false
+        };
+
+        this.save();
+    }
+
+};
+
+Settings.load();
