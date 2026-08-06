aff73e02ed1ad529d48d0f22e48694e35adf8eac
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:36:30 -0700

    Build V5 foundation - reports module

--- reports.js
+++ reports.js
@@ -1,1 +1,35 @@
+const Reports = {
 
+    summary() {
+
+        return {
+            totalEvents: Events.all().length,
+            totalCustomers: CRM.all().length,
+            totalVendors: Vendors.all().length,
+            totalVenues: Venues.all().length,
+            totalInventoryItems: Inventory.all().length,
+            totalAssets: Assets.all().length,
+            totalIncome: Finance.income(),
+            totalExpenses: Finance.expenses(),
+            totalProfit: Finance.profit()
+        };
+
+    },
+
+    dashboard() {
+
+        const data = this.summary();
+
+        console.table(data);
+
+        return data;
+
+    },
+
+    export() {
+
+        return JSON.stringify(this.summary(), null, 2);
+
+    }
+
+};
