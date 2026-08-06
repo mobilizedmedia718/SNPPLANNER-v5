fd2df4a36c0102ce8ff2dc6bf315db45ae598e30
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 16:13:40 -0700

    Fix settings module syntax

--- settings.js
+++ settings.js
@@ -11,35 +11,35 @@ const Settings = {
 
     load() {
         this.data = Utils.load("settings", this.data);
-   if (this.data.theme === "dark") {
-    document.body.classList.add("dark");
-} else {
-    document.body.classList.remove("dark");
-   }
-    },
 
-    save() {
-        Utils.save("settings", this.data);
-    }
-    },
-
-    update(key, value) {
-
-    if (!(key in this.data)) return;
-
-    this.data[key] = value;
-
-    this.save();
-
-    if (key === "theme") {
-
-        if (value === "dark") {
+        if (this.data.theme === "dark") {
             document.body.classList.add("dark");
         } else {
             document.body.classList.remove("dark");
         }
+    },
 
-    }
+    save() {
+        Utils.save("settings", this.data);
+    },
+
+    update(key, value) {
+
+        if (!(key in this.data)) return;
+
+        this.data[key] = value;
+
+        this.save();
+
+        if (key === "theme") {
+
+            if (value === "dark") {
+                document.body.classList.add("dark");
+            } else {
+                document.body.classList.remove("dark");
+            }
+
+        }
 
     },
 
