dd3cadfd4c2c6ee08b7a1fa506cc0def41963001
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:21:12 -0700

    Build V5 foundation - business module

--- business.js
+++ business.js
@@ -1,1 +1,54 @@
+const Business = {
 
+    data: {
+        name: "",
+        owner: "",
+        logo: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+        phone: "",
+        email: "",
+        website: "",
+        taxRate: 0,
+        notes: ""
+    },
+
+    load() {
+        this.data = Utils.load("business", this.data);
+    },
+
+    save() {
+        Utils.save("business", this.data);
+    },
+
+    update(field, value) {
+        if (field in this.data) {
+            this.data[field] = value;
+            this.save();
+        }
+    },
+
+    reset() {
+        this.data = {
+            name: "",
+            owner: "",
+            logo: "",
+            address: "",
+            city: "",
+            state: "",
+            zip: "",
+            phone: "",
+            email: "",
+            website: "",
+            taxRate: 0,
+            notes: ""
+        };
+
+        this.save();
+    }
+
+};
+
+Business.load();
