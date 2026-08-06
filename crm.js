526f6b63cb961d3a8701c2b1d7a9e95d6aca42d3
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 14:30:36 -0700

    Build V5 foundation - CRM module
    Implement CRM functionality for customer management including create, update, remove, and search methods.

--- crm.js
+++ crm.js
@@ -1,1 +1,80 @@
+const CRM = {
 
+    customers: [],
+
+    load() {
+        this.customers = Utils.load("customers", []);
+    },
+
+    save() {
+        Utils.save("customers", this.customers);
+    },
+
+    create(data = {}) {
+
+        const customer = {
+            id: Utils.id(),
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
+            birthday: "",
+            address: "",
+            city: "",
+            state: "",
+            zip: "",
+            loyaltyPoints: 0,
+            totalSpent: 0,
+            totalVisits: 0,
+            tags: [],
+            notes: "",
+            created: Utils.date(),
+            ...data
+        };
+
+        this.customers.push(customer);
+        this.save();
+
+        return customer;
+    },
+
+    update(id, updates) {
+
+        const customer = this.customers.find(c => c.id === id);
+
+        if (!customer) return;
+
+        Object.assign(customer, updates);
+
+        this.save();
+    },
+
+    remove(id) {
+
+        this.customers = this.customers.filter(c => c.id !== id);
+
+        this.save();
+    },
+
+    get(id) {
+        return this.customers.find(c => c.id === id);
+    },
+
+    all() {
+        return this.customers;
+    },
+
+    search(term) {
+
+        term = term.toLowerCase();
+
+        return this.customers.filter(c =>
+            `${c.firstName} ${c.lastName} ${c.email} ${c.phone}`
+                .toLowerCase()
+                .includes(term)
+        );
+    }
+
+};
+
+CRM.load();
