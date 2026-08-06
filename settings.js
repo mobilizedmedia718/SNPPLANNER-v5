7fde374df00d0797581bac188f4f09cdd7b99f6a
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 15:42:57 -0700

    Enable dark mode switching
    Added theme handling in load and update methods.

--- settings.js
+++ settings.js
@@ -11,19 +11,36 @@ const Settings = {
 
     load() {
         this.data = Utils.load("settings", this.data);
+   if (this.data.theme === "dark") {
+    document.body.classList.add("dark");
+} else {
+    document.body.classList.remove("dark");
+   }
     },
 
     save() {
         Utils.save("settings", this.data);
+    }
     },
 
     update(key, value) {
 
-        if (!(key in this.data)) return;
+    if (!(key in this.data)) return;
 
-        this.data[key] = value;
+    this.data[key] = value;
 
-        this.save();
+    this.save();
+
+    if (key === "theme") {
+
+        if (value === "dark") {
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+        }
+
+    }
+
     },
 
     reset() {
