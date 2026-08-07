175f29141b51e5ca70f175504b8da96078a3477b
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-06 17:41:23 -0700

    Fix Events module compatibility
    Added create and update methods for event management.

--- events.js
+++ events.js
@@ -10,19 +10,51 @@ const Events = {
         Utils.save("events", this.data);
     },
 
-    all() {
-        return this.data;
+    create(eventData = {}) {
+
+        const event = {
+            id: Utils.id(),
+            name: "",
+            date: "",
+            time: "",
+            venueId: "",
+            status: "Draft",
+            capacity: 0,
+            ticketsSold: 0,
+            notes: "",
+            created: Utils.date(),
+            ...eventData
+        };
+
+        this.data.push(event);
+        this.save();
+
+        return event;
     },
 
-    add(event) {
-        event.id = Utils.id();
-        this.data.push(event);
+    update(id, updates) {
+
+        const event = this.data.find(e => e.id === id);
+
+        if (!event) return;
+
+        Object.assign(event, updates);
         this.save();
     },
 
     remove(id) {
         this.data = this.data.filter(e => e.id !== id);
         this.save();
+    },
+
+    get(id) {
+        return this.data.find(e => e.id === id);
+    },
+
+    all() {
+        return this.data;
     }
 
 };
+
+Events.load();
