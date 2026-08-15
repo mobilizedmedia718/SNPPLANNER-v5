577824e0579c733d74d718c86707038af345b4fd
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-15 01:28:58 -0700

    Bump cache for staff event portal

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "snpplanner-v5-22";
+const CACHE = "snpplanner-v5-23";
 const FILES = ["./styles.css","./manifest.json"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
