b9783d59dc0ec26d607f57cee2807471577e1162
mobilizedmedia718 <mobilizedmedia718@gmail.com>
2026-08-16 13:42:57 -0700

    Allocate couples package revenue: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "snpplanner-v5-26";
+const CACHE = "snpplanner-v5-27";
 const FILES = ["./styles.css","./manifest.json"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
